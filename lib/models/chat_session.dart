9512985b70a82f9240a49c3c3454c78c79641735
huunguyn099 <huunguyn099@gmail.com>
2026-08-24 10:58:49 +0700

    Create chat_session.dart

---
+++ lib/models/chat_session.dart
@@ -0,0 +1,31 @@
+import 'chat_message.dart';
+
+class ChatSession {
+  final String id;
+  String title;
+  final DateTime createdAt;
+  List<ChatMessage> messages;
+
+  ChatSession({
+    required this.id,
+    required this.title,
+    required this.createdAt,
+    required this.messages,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'createdAt': createdAt.toIso8601String(),
+        'messages': messages.map((m) => m.toJson()).toList(),
+      };
+
+  factory ChatSession.fromJson(Map<String, dynamic> json) => ChatSession(
+        id: json['id'],
+        title: json['title'] ?? 'Đoạn chat mới',
+        createdAt: DateTime.parse(json['createdAt']),
+        messages: (json['messages'] as List)
+            .map((m) => ChatMessage.fromJson(m))
+            .toList(),
+      );
+}
